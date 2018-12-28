8d1b177684562c85b8ac88913ddc98acad67220a
duanckham <cong.duan@grabtaxi.com>
2018-12-28 16:27:43 +0800

    MISC

---
+++ README.md
@@ -0,0 +1,3 @@
+```
+npx pelf <project_name>
+```
--- cli.js
+++ cli.js
@@ -1,3 +1,20 @@
 #!/usr/bin/env node
 
-console.log(process.argv);
+const copydir = require('copy-dir');
+
+if (process.argv.length >= 3) {
+  let projectName = process.argv[2];
+
+  console.log('Processing...');
+  copydir.sync(`${__dirname}/materials`, `${process.env.PWD}/${projectName}`);
+  console.log('Done.\n');
+  console.log('  - Create development config file:');
+  console.log(`    \`mv ${projectName}/app/project/config/development.new.js ${projectName}/app/project/config/development.js\`\n`);
+  console.log('  - Start web (http://localhost:10001):');
+  console.log(`    \`cd ${projectName}/app/project && npm run build-dev\`\n`);
+  console.log('  - Start endpoints:');
+  console.log(`    \`cd ${projectName}/app/project && npm run start-endpoints\`\n`);
+  console.log('Have a good time.');
+} else {
+  console.log('Please provide the folder name of the project.');
+}
