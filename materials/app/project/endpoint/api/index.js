88bde109c52dfcccb2739a0ce6ffcdf77c4df6df
duanckham <cong.duan@grabtaxi.com>
2019-02-15 18:18:29 +0800

    MISC

--- materials/app/project/endpoint/api/index.js
+++ materials/app/project/endpoint/api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('ping', (req, res) => res.json('pong'));
+router.get('/ping', (req, res) => res.json('pong'));
 
 module.exports = router;
